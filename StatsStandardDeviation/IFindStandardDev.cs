578f915618a34c03a43f42a93f3c4ae3b5f05fb6
dana <dana.changwe@szi.gov.zm>
2020-01-15 20:50:27 +0200

    Feature: added an interface for Standard Deviation

---
+++ StatsStandardDeviation/IFindStandardDev.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StatsStandardDeviation
+{
+    public interface IFindStandardDev
+    {
+        public double StandardDev(dynamic arrayA);
+    }
+}
