a98d7aeae9d979ca384d82282c8ff6c0c8286b4d
dana <dana.changwe@szi.gov.zm>
2020-01-15 16:43:38 +0200

    Feature: added Variance Tests

---
+++ StatsVarianceTests/FindVarianceTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StatsVariance;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StatsVariance.Tests
+{
+    [TestClass()]
+    public class FindVarianceTests
+    {
+        public static FindVariance findVariance = new FindVariance();
+        [TestMethod()]
+        public void VarianceIntTest()
+        {
+            int[] values = { 2, 4, 6, 8 };
+            double variance = findVariance.Variance(values);
+            Assert.AreEqual(5, Helpers.Rounding.RoundFiveDecimals(variance));
+
+        }
+        [TestMethod()]
+        public void VarianceDoubleTest()
+        {
+            double[] values = { 2.0, 3.0, 4.0 };
+            double variance = findVariance.Variance(values);
+            Assert.AreEqual(0.66667, Helpers.Rounding.RoundFiveDecimals(variance));
+
+        }
+    }
+}
