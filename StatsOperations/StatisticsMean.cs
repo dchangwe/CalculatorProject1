39d9cc559718f0599fe1f9e06ce3845d362a8acf
dana <dana.changwe@szi.gov.zm>
2020-01-16 14:06:39 +0200

    Fix: removed error in Statistics Mean

--- StatsOperations/StatisticsMean.cs
+++ StatsOperations/StatisticsMean.cs
@@ -7,9 +7,9 @@ namespace StatsOperations
     {
         public static dynamic Mean(dynamic values)
         {
-            var sum = BasicOperations.Addition.Sum(values);
+            var sum = Addition.Sum(values);
             var valueCount = Helpers.Arrays.Length(values);
-            var result = BasicOperations.Division.Quotient(sum, valueCount);
+            var result = Division.Quotient(sum, valueCount);
             return result;
         }
     }
