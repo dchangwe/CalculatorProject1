071890e1e798520fcd4629fb46db140dbcf53f24
dana <dana.changwe@szi.gov.zm>
2020-01-17 11:59:06 +0200

    Fix : edited StatsOperations

--- StatsOperations/StatisticMean.cs
+++ StatsOperations/StatisticMean.cs
@@ -3,7 +3,7 @@ using BasicOperations;
 
 namespace StatsOperations
 {
-    public class StatisticsMean
+    public class StatisticMean
     {
         public static dynamic Mean(dynamic values)
         {
---
+++ StatsOperations/StatsZScore.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BasicOperations;
+namespace StatsOperations
+{
+ public  class StatsZScore
+    {
+        public static double StatZScore(dynamic score, dynamic values)
+        {
+            double mean = StatisticMean.Mean(values);
+            double standDev = StatStandardDev.StandardDev(values);
+            double zScore = Division.Quotient(Subtraction.Subtract(score, mean), standDev);
+            return zScore;
+        }
+    }
+}
